06a6a977e26343eb911f419ea66ad5ffbd21a113
Kouede Loic <kouedelo@gmail.com>
2022-04-24 19:23:05 -0400

    added peer to peer files to project

--- src/TCPPeerClient.java
+++ src/TCPPeerClient.java
@@ -23,7 +23,8 @@ public class TCPPeerClient extends Thread {
 
         boolean running = true;
 
-        String routerName = "DESKTOP-72USF14";
+        String routerName = /*"DESKTOP-72USF14";*/"DESKTOP-0EN1VER";
+
         // ServerRouter host name
 
         int SockNum = 5555;
@@ -44,30 +45,30 @@ public class TCPPeerClient extends Thread {
 
         // Variables for message passing
         String fromServer; // messages received from ServerRouter
-        String address = "192.168.137.210"; // destination IP (Server)---modify
+        String address = "192.168.137.1"; // destination IP (Server)---modify
         long t0, t1, t;
 
 
-        final int SOCKET_PORT = 13267;  // you may change this
-        final String FILE_TO_BE_RECEIVED = "C:\\Users\\Kouede Loic\\OneDrive\\Desktop\\file2.txt";  // you may change this
+        final int SOCKET_PORT = 15000;  // you may change this
+        final String FILE_TO_BE_RECEIVED = "C:\\Users\\Kouede Loic\\OneDrive\\Desktop\\file2.png";  // you may change this
 
         // Communication process (initial sends/receives
         out.println(address);// initial send (IP of the destination Server)
         fromServer = in.readLine();//initial receive from router (verification of connection)
         System.out.println("ServerRouter: " + fromServer);
 
-        boolean foundDestination = false;
+//        boolean foundDestination = false;
+//
+//        System.out.println("Waiting for Server Router's response...");
+//        while ((fromServer = in.readLine()) != null) {
+//            System.out.println("Server Router said: " + fromServer);
+//            if (fromServer.equals(address)) { // exit statement
+//                foundDestination = true;
+//                break;
+//            }
+//        }
 
-        System.out.println("Waiting for Server Router's response...");
-        while ((fromServer = in.readLine()) != null) {
-            System.out.println("Server Router said: " + fromServer);
-            if (fromServer.equals(address)) { // exit statement
-                foundDestination = true;
-                break;
-            }
-        }
-
-        if (foundDestination) {
+        //if (foundDestination) {
             System.out.println("Starting connection with " + address);
 
 
@@ -122,7 +123,7 @@ public class TCPPeerClient extends Thread {
             out.close();
             in.close();
             Socket.close();
-        }
+        //}
     }
 
 
